e859147d85e21d1c395462e7e746627668e3f895
Doan Ngoc Binh Minh <bmmit.work@gmail.com>
2026-03-14 01:23:09 +0700

    add zsh configurations

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,9 @@
-eval "$(starship init zsh)"
+ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
+ZINIT_HOME="$ZSH_CONFIG/zinit"
+
+export PATH="$HOME/.local/bin:$PATH"
+
+# Zinit plugin manager
+source "$ZINIT_HOME/zinit.zsh"
+source "$ZSH_CONFIG/plugins.zsh"
+
---
+++ zsh/plugins.zsh
@@ -0,0 +1,14 @@
+ZOXIDE_CMD_OVERRIDE=cd
+
+zi snippet OMZP::git
+zi snippet OMZP::node
+zi snippet OMZP::nvm
+zi snippet OMZP::npm
+zi snippet OMZP::python
+zi snippet OMZP::starship
+zi snippet OMZP::yarn
+zi snippet OMZP::zoxide
+
+zi light z-shell/zsh-eza
+zi light zsh-users/zsh-autosuggestions
+zi light zsh-users/zsh-syntax-highlighting
